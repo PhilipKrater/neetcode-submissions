ac063af70356c4546ce01eca820ff38003e654c7
PhilipKrater <p.konta@web.de>
2026-09-13 15:07:26 +0200

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] counts = new int[26];
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+
+        for (char c : sChars) {
+            counts[c - 'a']++;
+        }
+
+        for (char c : tChars) {
+            if (--counts[c - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
